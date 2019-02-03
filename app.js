d939bb9cdddf9edbbfac378d4ca10ef9b9a1edb8
Galvanize Web Development <Galvanize@Isaacs-MacBook-Pro.local>
2019-02-03 11:14:47 -0700

    fixed port

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const indexRouter = require('./mvc/routes/routes');
-const path=require('path')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cors = require("cors")
+const indexRouter = require('./mvc/routes/routes.js')
+const app = express()
 
-app.disable('x-powered-by')
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(cors())
-app.use('/movies', indexRouter);
+app.use('/movies', indexRouter)
 
-
-module.exports = app;
+module.exports = app
